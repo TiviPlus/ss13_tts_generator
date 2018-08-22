a6662e1f855670219b51acea157b96853ce567ac
JohnGinnane <John.Ginnane1@Gmail.com>
2018-08-22 14:29:09 +0100

    Added lock file usage and creating .meta file which has length of audio inside

--- tts_generator/Program.cs
+++ tts_generator/Program.cs
@@ -6,13 +6,39 @@ using System.Threading.Tasks;
 using SharpTalk;
 using System.IO;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace tts_generator
 {
+    /* Solution: https://stackoverflow.com/a/82408 */
+    public static class SoundInfo
+    {
+        [DllImport("winmm.dll")]
+        private static extern uint mciSendString(
+            string command,
+            StringBuilder returnValue,
+            int returnlength,
+            IntPtr winHandle);
+
+        public static int GetSoundLength(string fileName)
+        {
+            StringBuilder lengthBuf = new StringBuilder(32);
+
+            mciSendString(string.Format("open \"{0}\" type waveaudio alias wave", fileName), null, 0, IntPtr.Zero);
+            mciSendString("status wave length", lengthBuf, lengthBuf.Capacity, IntPtr.Zero);
+            mciSendString("close wave", null, 0, IntPtr.Zero);
+
+            int length = 0;
+            int.TryParse(lengthBuf.ToString(), out length);
+
+            return length;
+        }
+    }
+
     class Program
     {
         private static String ConverterProgram = "OggEnc.exe";
-        
+
         static void Main(string[] args)
         {
             if (args.Length <= 0)
@@ -109,17 +135,48 @@ namespace tts_generator
                 outfile = "speech.wav";
             }
 
-            outfile = outfile.Replace("\"", "");
+            char[] charArray = outfile.ToCharArray();
+            Array.Reverse(charArray);
+            String outfileExtensionless = new string(charArray);
+            int dotIndex = outfileExtensionless.IndexOf(".");
+            outfileExtensionless = outfileExtensionless.Substring(dotIndex + 1);
+            charArray = outfileExtensionless.ToCharArray();
+            Array.Reverse(charArray);
+            outfileExtensionless = new string(charArray);
 
+            /* 
+             * The process for generating TTS:
+             * 1. Create .lock file
+             * 2. Create .wav file
+             * 3. Convert to .ogg
+             * 4. Create .data file (containing audio length)
+             * 5. Delete .lock file
+             */
+
+            /* Lock file */
+            String lockFile = outfileExtensionless + ".lock";
+            FileStream fs = File.Create(programPath + lockFile);
+            fs.Close();            
+
+            /* Wav file */
+            outfile = outfile.Replace("\"", "");
             fte.SpeakToWavFile(programPath + outfile, message);
             
-            Process converter = new Process();
-            
+            /* Ogg file */
+            Process converter = new Process();            
             converter.StartInfo.FileName = programPath + ConverterProgram;
             converter.StartInfo.Arguments = "\"" + programPath + outfile + "\"";
             converter.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             converter.Start();
-            
+
+            /* Metadata file */
+            String metaFile = outfileExtensionless + ".meta";
+            int length = SoundInfo.GetSoundLength(programPath + outfile);
+            File.WriteAllText(programPath + metaFile, length.ToString());
+
+            /* Delete lock */
+            File.Delete(programPath + lockFile);
+
             Environment.Exit(0);
         }
     }
